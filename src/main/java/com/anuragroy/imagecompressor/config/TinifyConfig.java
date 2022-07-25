c180144af493e9691e1513c543b39bb99c6b61c0
Anurag Roy <anuragroy17@gmail.com>
2022-07-25 19:15:18 +0530

    throw exceptions is API key error

--- src/main/java/com/anuragroy/imagecompressor/config/TinifyConfig.java
+++ src/main/java/com/anuragroy/imagecompressor/config/TinifyConfig.java
@@ -1,15 +1,25 @@
 package com.anuragroy.imagecompressor.config;
 
 import com.tinify.Tinify;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
-public class TinifyConfig {
+public class TinifyConfig implements InitializingBean{
 
-    @PostConstruct
-    public void init() {
-        Tinify.setKey(System.getenv("TINIFY_API_KEY"));
+    public void afterPropertiesSet() {
+        String apiKey = System.getenv("TINIFY_API_KEY");
+        if (StringUtils.isBlank(apiKey)) {
+            throw new IllegalArgumentException("TINIFY_API_KEY must be configured");
+        }
+        try {
+            Tinify.setKey(apiKey);
+            Tinify.validate();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Failed to validate API Key");
+        }
     }
 }
