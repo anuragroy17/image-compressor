7e18625031ff417d217c4210cd39703ac3aa671e
Anurag Roy <anuragroy17@gmail.com>
2022-07-25 16:51:01 +0530

    initial commit

---
+++ src/test/java/com/anuragroy/imagecompressor/ImageCompressorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuragroy.imagecompressor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageCompressorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
